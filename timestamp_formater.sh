8e55806be3306dad4e069acdc9c2571b80c91dbb
Fu Lee <flee@Fu-Lee-MacBook-Pro.local>
2019-10-17 15:12:18 -0400

    Modify David's sed change to have conditional strip off double quotation

--- timestamp_formater.sh
+++ timestamp_formater.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 Help() {
-   echo "`basename $0` is a script to add human readable timestamp at left most column to the original log file. "
-   echo "Usage: `basename $0` -t [type] -f [input_log_file]"
+   echo "`basename $0` is a script to insert human readable timestamp at left most column to the original log file. "
+   echo "Usage: `basename $0` -f [input_log_file]"
    echo " "
    echo "   <Note:> "
-   echo "           <required> -t [type] gorouter or autioneer "
-   echo "           <required> -f [input_log_file] gorouter.stdout.log or auctioneer.stdout.log "
+   echo "           <required> -f [input_log_file] gorouter.stdout.log,  auctioneer.stdout.log "
    echo ""
-   echo "  e.g. ./`basename $0` -t gorouter gorouter.stdout.log"
-   echo "       ./`basename $0` -t autioneer autioneer.stdout.log"
+   echo "  e.g. ./`basename $0` -f gorouter.stdout.log"
+   echo "       ./`basename $0` -f autioneer.stdout.log"
    exit 1
 }
 
 # Check input parameters
-while getopts ":f:h:t:" option;
+while getopts ":f:h:" option;
 do
    case "$option" in
       f) inputfile=$OPTARG;;
       h) Help;;
-      t) type=$OPTARG;;
       *) Help
          exit 1;;
    esac
 done
+
+if [ -z ${inputfile} ]; then
+     Help
+fi
 
 outfile=$inputfile.new
 if [ -e ${outfile} ]; then
@@ -31,14 +33,7 @@ fi
 
 while IFS= read -r line
 do
-   if [ $type  == "gorouter" ] ; then
-     temptime=`echo $line | awk -F'timestamp"' ' { print $2 }' | sed 's/^://' | sed -n 's/\([0-9]\{10\}\).*/\1/p'` 
-   elif [ $type  == "autioneer" ] ; then
-     temptime=`echo $line | awk -F'timestamp"' ' { print $2 }' | sed 's/^://' | sed -n 's/\([0-9]\{10\}\).*/\1/p' | sed 's/"//g'` 
-   else  
-     echo "           <required> -t [type] gorouter or autioneer "
-     exit 1
-   fi  
+   temptime=`echo $line | awk -F'timestamp"' ' { print $2 }' | sed -E 's/^(:"|:)//' | sed -n 's/\([0-9]\{10\}\).*/\1/p'`
    varlength=${#temptime} 
    if [ $varlength  -ne 0 ] ; then
       newtime=`date -r $temptime`
