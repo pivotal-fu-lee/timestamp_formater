b58bdbed42e02d05de207d97b98f6ad34fa7b760
Fu Lee <flee@Fu-Lee-MacBook-Pro.local>
2019-10-17 10:34:46 -0400

    Add -t for different types of log files and some clean up

--- timestamp_formater.sh
+++ timestamp_formater.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
-# set -x 
-outfile=$1.new
 Help() {
    echo "`basename $0` is a script to add human readable timestamp at left most column to the original log file. "
-   echo "" 
-   echo "        Usage: `basename $0` gorouter.stdout.log"
+   echo "Usage: `basename $0` -t [type] -f [input_log_file]"
+   echo " "
+   echo "   <Note:> "
+   echo "           <required> -t [type] gorouter or autioneer "
+   echo "           <required> -f [input_log_file] gorouter.stdout.log or auctioneer.stdout.log "
+   echo ""
+   echo "  e.g. ./`basename $0` -t gorouter gorouter.stdout.log"
+   echo "       ./`basename $0` -t autioneer autioneer.stdout.log"
    exit 1
 }
-if [ $# -ne 1 ] ; then
-   Help 
-fi
-if [ $1 == "-h" ] ; then
-   Help 
-fi
+
+# Check input parameters
+while getopts ":f:h:t:" option;
+do
+   case "$option" in
+      f) inputfile=$OPTARG;;
+      h) Help;;
+      t) type=$OPTARG;;
+      *) Help
+         exit 1;;
+   esac
+done
+
+outfile=$inputfile.new
 if [ -e ${outfile} ]; then
     rm ${outfile} 
 fi
+
 while IFS= read -r line
 do
-#  echo $line | awk -F'timestamp"' ' { print $2 }' | sed 's/://g' | sed -n 's/\([0-9]\{10\}\).*/\1/p' | xargs -I dummy sh -c "date -r dummy" >> $outfile
-   # temptime=`echo $line | awk -F'timestamp"' ' { print $2 }' | sed 's/://g' | sed -n 's/\([0-9]\{10\}\).*/\1/p'` 
-   temptime=`echo $line | awk -F'timestamp"' ' { print $2 }' | sed 's/^://' | sed -n 's/\([0-9]\{10\}\).*/\1/p'` 
+   if [ $type  == "gorouter" ] ; then
+     temptime=`echo $line | awk -F'timestamp"' ' { print $2 }' | sed 's/^://' | sed -n 's/\([0-9]\{10\}\).*/\1/p'` 
+   elif [ $type  == "autioneer" ] ; then
+     temptime=`echo $line | awk -F'timestamp"' ' { print $2 }' | sed 's/^://' | sed -n 's/\([0-9]\{10\}\).*/\1/p' | sed 's/"//g'` 
+   else  
+     echo "           <required> -t [type] gorouter or autioneer "
+     exit 1
+   fi  
    varlength=${#temptime} 
-#   echo " variable length: $varlength"
    if [ $varlength  -ne 0 ] ; then
       newtime=`date -r $temptime`
       echo $newtime $line >> $outfile  
    else   
       echo $line >> $outfile  
    fi
-done < $1
-# paste $outfile $1 | column -s $'\t' -t > $mergefile
+done < $inputfile
